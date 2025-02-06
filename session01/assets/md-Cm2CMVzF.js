b6d841da5e95ad9d3778e940728a44317818abb7
Alok Menghrajani <441307+alokmenghrajani@users.noreply.github.com>
2025-02-06 13:17:42 +0100

    session01

---
+++ session01/assets/md-Cm2CMVzF.js
@@ -0,0 +1,6 @@
+import{_ as l}from"./slidev/VClicks-CUclCijb.js";import{o as i,c as m,k as a,e,l as c,ac as u,g as p,q as f,s as d,B as o}from"./modules/vue-2Gl6h_QT.js";import{I as _}from"./slidev/default-CwdJ12WO.js";import{u as h,f as k}from"./slidev/context-BR9WBF9I.js";import"./index--ATcJbzm.js";import"./modules/shiki-so3_fRfh.js";const w={__name:"session01.md__slidev_3",setup(g){const{$slidev:x,$nav:$,$clicksContext:n,$clicks:v,$page:C,$renderContext:B,$frontmatter:r}=h();return n.setup(),(N,t)=>{const s=l;return i(),m(_,f(d(o(k)(o(r),2))),{default:a(()=>[t[1]||(t[1]=e("h1",null,"Questions, comments, feedback:",-1)),c(s,null,{default:a(()=>[t[0]||(t[0]=e("ul",null,[e("li",null,"Forum"),e("li",null,[e("a",{href:"mailto:alok.menghrajani@epfl.ch",target:"_blank"},"alok.menghrajani@epfl.ch"),u(" or "),e("a",{href:"mailto:alexandre.alahi@epfl.ch",target:"_blank"},"alexandre.alahi@epfl.ch")]),e("li",null,"Teaching Assistants")],-1)),p(`
+
+This is the first time we are doing this course. We are very interested
+to hear from you.
+
+`)]),_:1})]),_:1},16)}}};export{w as default};
---
+++ session01/assets/slidev/default-CwdJ12WO.js
@@ -0,0 +1,1 @@
+import{o,b as t,f as s}from"../modules/vue-2Gl6h_QT.js";import{_ as r}from"../index--ATcJbzm.js";const c={},a={class:"slidev-layout default"};function n(e,l){return o(),t("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
